fba4ec7474f828eaf39de8b5d2e915d3398121a4
umbrellaplug <107957815+umbrellaplug@users.noreply.github.com>
2022-07-04 18:34:02 -0500

    401 Trakt Fix for Lists

--- matrix/plugin.video.umbrella/resources/lib/modules/trakt.py
+++ matrix/plugin.video.umbrella/resources/lib/modules/trakt.py
@@ -61,6 +61,9 @@ def getTrakt(url, post=None, extended=False, silent=False):
 			if extended: return response, response.headers
 			else: return response
 		elif status_code == '401': # Re-Auth token
+			if response.headers["x-private-user"] == 'true':
+				log_utils.log('X-Private-User Returned from URL:%s Cannot return these values.' % url, level=log_utils.LOGWARNING)
+				return None
 			success = re_auth(headers)
 			if success: return getTrakt(url, extended=extended, silent=silent)
 		elif status_code == '429':
