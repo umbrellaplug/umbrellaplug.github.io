1e8c4c4a634e3786e4f49860a922814bb8e3ed78
umbrellaplug <107957815+umbrellaplug@users.noreply.github.com>
2023-01-18 22:55:13 -0600

    Update player.py
    changed to check if setting is set then use if set.

--- matrix/plugin.video.umbrella/resources/lib/modules/player.py
+++ matrix/plugin.video.umbrella/resources/lib/modules/player.py
@@ -748,8 +748,9 @@ class Subtitles:
 			content = server.DownloadSubtitles(token, content)
 			content = base64.b64decode(content['data'][0]['data'])
 			content = gzip.GzipFile(fileobj=BytesIO(content)).read()
-			download_path = getSetting('subtitles.custompath') if getSetting('subtitles.custompath') else 'special://temp/'
-			subtitle = control.transPath(download_path)
+			download_path = jsloads(control.jsonrpc('{"jsonrpc":"2.0", "method":"Settings.GetSettingValue", "params":{"setting":"subtitles.custompath"}, "id":1}')).get('result').get('value')
+			download_path = 'special://subtitles/' if download_path is not '' else 'special://temp/'
+			subtitle = control.transPath('download_path')
 			subtitle = control.joinPath(subtitle, 'TemporarySubs.%s.srt' % lang)
 			log_utils.log('subtitle file = %s' % subtitle, level=log_utils.LOGDEBUG)
 
